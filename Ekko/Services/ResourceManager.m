8a75c0e8f6a353a88b00ad67064d81b1d03e7d25
Brian Zoetewey <brian.zoetewey@ccci.org>
2014-01-10 15:14:26 -0500

    Handle NSManagedObject properly when passed between threads

--- Ekko/Services/ResourceManager.m
+++ Ekko/Services/ResourceManager.m
@@ -7,6 +7,7 @@
 //
 
 #import "ResourceManager.h"
+#import "DataManager.h"
 #import "CourseIdProtocol.h"
 #import "HubClient.h"
 #import <AFHTTPRequestOperation.h>
@@ -14,13 +15,11 @@
 #import "UIImage+Ekko.h"
 
 NSString *const kEkkoResourceManagerImageCache = @"org.ekkoproject.ios.player.imagecache";
-const char * kEkkoResourceManagerDispatchQueue = "org.ekkoproject.ios.player.resourcemanager.queue";
 NSString *const kEkkoResourceManagerCacheDirectoryName = @"org.ekkoproject.ios.player";
 
 @interface ResourceManager ()
 @property (nonatomic, strong) NSString *cacheDirectory;
 @property (nonatomic, strong) NSCache *imageCache;
-@property (nonatomic, strong, readonly) dispatch_queue_t dispatchQueue;
 @end
 
 @implementation ResourceManager
@@ -50,9 +49,23 @@ NSString *const kEkkoResourceManagerCacheDirectoryName = @"org.ekkoproject.ios.p
     return self;
 }
 
+-(void)getImageResource:(NSString *)resourceId
+               courseId:(NSString *)courseId
+                   sha1:(NSString *)sha1
+          completeBlock:(void (^)(NSString *courseId, NSString *resourceId, UIImage *))completeBlock {
+}
+
+
 -(void)getImageResource:(Resource *)resource completeBlock:(void (^)(Resource *, UIImage *))completeBlock {
-    dispatch_async(self.dispatchQueue, ^{
-        NSString *cacheKey = [self cacheKeyForResource:resource];
+    NSManagedObjectContext *privateContext = [[DataManager sharedManager] newPrivateQueueManagedObjectContext];
+    [privateContext performBlock:^{
+        NSError *error;
+        Resource *_resource = (Resource *)[privateContext existingObjectWithID:resource.objectID error:&error];
+        if (error) {
+            return;
+        }
+        NSString *cacheKey = [self cacheKeyForResource:_resource];
+        NSString *path = [self pathForResource:_resource];
 
         //Try to load image from cache
         UIImage *image = (UIImage *)[self.imageCache objectForKey:cacheKey];
@@ -60,9 +73,9 @@ NSString *const kEkkoResourceManagerCacheDirectoryName = @"org.ekkoproject.ios.p
             completeBlock(resource, image);
             return;
         }
-        
+
         //Load image from disk and cache if it exists
-        NSData *data = [NSKeyedUnarchiver unarchiveObjectWithFile:[self pathForResource:resource]];
+        NSData *data = [NSKeyedUnarchiver unarchiveObjectWithFile:path];
         if (data) {
             image = [UIImage inflatedImage:data scale:[UIScreen mainScreen].scale];
             if (image) {
@@ -71,41 +84,41 @@ NSString *const kEkkoResourceManagerCacheDirectoryName = @"org.ekkoproject.ios.p
                 return;
             }
         }
-        
+
         //Download image if not in cache or disk
-        if ([resource isFile]) {
-            [[HubClient sharedClient] getResource:resource.courseId sha1:resource.sha1 callback:^(NSData *data) {
+        if ([_resource isFile]) {
+            [[HubClient sharedClient] getResource:_resource.courseId sha1:resource.sha1 callback:^(NSData *data) {
                 UIImage *image = [UIImage inflatedImage:data scale:[UIScreen mainScreen].scale];
                 if (image) {
                     completeBlock(resource, image);
                     [self.imageCache setObject:image forKey:cacheKey];
-                    [NSKeyedArchiver archiveRootObject:UIImagePNGRepresentation(image) toFile:[self pathForResource:resource]];
+                    [NSKeyedArchiver archiveRootObject:UIImagePNGRepresentation(image) toFile:path];
                 }
             }];
         }
-        else if ([resource isUri]) {
-            NSMutableURLRequest *request = [NSMutableURLRequest requestWithURL:[NSURL URLWithString:resource.uri] cachePolicy:NSURLRequestReloadIgnoringCacheData timeoutInterval:20];
+        else if ([_resource isUri]) {
+            NSMutableURLRequest *request = [NSMutableURLRequest requestWithURL:[NSURL URLWithString:_resource.uri] cachePolicy:NSURLRequestReloadIgnoringCacheData timeoutInterval:20];
             AFHTTPRequestOperation * operation = [[HubClient sharedClient] HTTPRequestOperationWithRequest:request success:^(AFHTTPRequestOperation *operation, id responseObject) {
                 if (responseObject && [responseObject isKindOfClass:[UIImage class]]) {
                     UIImage *image = (UIImage *)responseObject;
                     completeBlock(resource, image);
                     [self.imageCache setObject:image forKey:cacheKey];
-                    [NSKeyedArchiver archiveRootObject:UIImagePNGRepresentation(image) toFile:[self pathForResource:resource]];
+                    [NSKeyedArchiver archiveRootObject:UIImagePNGRepresentation(image) toFile:path];
                 }
             } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
             }];
             [operation setResponseSerializer:[AFImageResponseSerializer serializer]];
             [operation start];
         }
-        else if ([resource isEkkoCloudVideo]) {
-            [[HubClient sharedClient] getECVResourceURL:resource.courseId videoId:resource.videoId urlType:EkkoCloudVideoURLTypeThumbnail complete:^(NSURL *videoURL) {
+        else if ([_resource isEkkoCloudVideo]) {
+            [[HubClient sharedClient] getECVResourceURL:_resource.courseId videoId:_resource.videoId urlType:EkkoCloudVideoURLTypeThumbnail complete:^(NSURL *videoURL) {
                 NSMutableURLRequest *request = [NSMutableURLRequest requestWithURL:videoURL cachePolicy:NSURLRequestReloadIgnoringCacheData timeoutInterval:20];
                 AFHTTPRequestOperation *operation = [[HubClient sharedClient] HTTPRequestOperationWithRequest:request success:^(AFHTTPRequestOperation *operation, id responseObject) {
                     if (responseObject && [responseObject isKindOfClass:[UIImage class]]) {
                         UIImage *image = (UIImage *)responseObject;
                         completeBlock(resource, image);
                         [self.imageCache setObject:image forKey:cacheKey];
-                        [NSKeyedArchiver archiveRootObject:UIImagePNGRepresentation(image) toFile:[self pathForResource:resource]];
+                        [NSKeyedArchiver archiveRootObject:UIImagePNGRepresentation(image) toFile:path];
                     }
                 } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
                 }];
@@ -113,48 +126,57 @@ NSString *const kEkkoResourceManagerCacheDirectoryName = @"org.ekkoproject.ios.p
                 [operation start];
             }];
         }
-    });
+
+    }];
 }
 
 -(void)getResource:(Resource *)resource progressBlock:(void (^)(Resource *, float))progressBlock completeBlock:(void (^)(Resource *, NSString *))completeBlock {
-    dispatch_async(self.dispatchQueue, ^{
-        if ([resource isFile]) {
-            NSString *path = [self pathForResource:resource];
+    NSManagedObjectContext *privateContext = [[DataManager sharedManager] newPrivateQueueManagedObjectContext];
+    [privateContext performBlock:^{
+        NSError *error;
+        Resource *_resource = (Resource *)[privateContext existingObjectWithID:resource.objectID error:&error];
+        if (error) {
+            return;
+        }
+        if ([_resource isFile]) {
+            NSString *path = [self pathForResource:_resource];
             if ([[NSFileManager defaultManager] fileExistsAtPath:path]) {
                 completeBlock(resource, [path copy]);
             }
             else {
-                [[HubClient sharedClient] getResource:resource.courseId sha1:resource.sha1 outputStream:[NSOutputStream outputStreamToFileAtPath:path append:NO] progress:^(float progress) {
+                [[HubClient sharedClient] getResource:_resource.courseId sha1:_resource.sha1 outputStream:[NSOutputStream outputStreamToFileAtPath:path append:NO] progress:^(float progress) {
                     progressBlock(resource, progress);
                 } complete:^{
                     completeBlock(resource, path);
                 }];
             }
         }
-        else if ([resource isEkkoCloudVideo]) {
-            [[HubClient sharedClient] getECVResourceURL:resource.courseId videoId:resource.videoId urlType:EkkoCloudVideoURLTypeDownload complete:^(NSURL *videoURL) {
-                NSString *path = [[self pathForResource:resource] stringByAppendingPathExtension:[videoURL pathExtension]];
-                if ([[NSFileManager defaultManager] fileExistsAtPath:path]) {
-                    completeBlock(resource, [path copy]);
-                }
-                else {
-                    NSMutableURLRequest *request = [NSMutableURLRequest requestWithURL:videoURL];
-                    AFHTTPRequestOperation *operation = [[HubClient sharedClient] HTTPRequestOperationWithRequest:request success:^(AFHTTPRequestOperation *operation, id responseObject) {
-                        completeBlock(resource, path);
-                    } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
-                        
-                    }];
-                    [operation setResponseSerializer:[AFHTTPResponseSerializer serializer]];
-                    [operation setOutputStream:[NSOutputStream outputStreamToFileAtPath:path append:NO]];
-                    [operation setDownloadProgressBlock:^(NSUInteger bytesRead, long long totalBytesRead, long long totalBytesExpectedToRead) {
-                        float progress = totalBytesRead / (float)totalBytesExpectedToRead;
-                        progressBlock(resource, progress);
-                    }];
-                    [operation start];
-                }
+        else if ([_resource isEkkoCloudVideo]) {
+            [[HubClient sharedClient] getECVResourceURL:_resource.courseId videoId:_resource.videoId urlType:EkkoCloudVideoURLTypeDownload complete:^(NSURL *videoURL) {
+                [privateContext performBlock:^{
+                    NSString *path = [[self pathForResource:_resource] stringByAppendingPathExtension:[videoURL pathExtension]];
+                    if ([[NSFileManager defaultManager] fileExistsAtPath:path]) {
+                        completeBlock(resource, [path copy]);
+                    }
+                    else {
+                        NSMutableURLRequest *request = [NSMutableURLRequest requestWithURL:videoURL];
+                        AFHTTPRequestOperation *operation = [[HubClient sharedClient] HTTPRequestOperationWithRequest:request success:^(AFHTTPRequestOperation *operation, id responseObject) {
+                            completeBlock(resource, path);
+                        } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
+
+                        }];
+                        [operation setResponseSerializer:[AFHTTPResponseSerializer serializer]];
+                        [operation setOutputStream:[NSOutputStream outputStreamToFileAtPath:path append:NO]];
+                        [operation setDownloadProgressBlock:^(NSUInteger bytesRead, long long totalBytesRead, long long totalBytesExpectedToRead) {
+                            float progress = totalBytesRead / (float)totalBytesExpectedToRead;
+                            progressBlock(resource, progress);
+                        }];
+                        [operation start];
+                    }
+                }];
             }];
         }
-    });
+    }];
 }
 
 -(void)getResource:(Resource *)resource delegate:(__weak id<ResourceManagerDelegate>)delegate {
@@ -188,15 +210,6 @@ NSString *const kEkkoResourceManagerCacheDirectoryName = @"org.ekkoproject.ios.p
 
 -(NSString *)cacheKeyForResource:(Resource *)resource {
     return [resource.courseId stringByAppendingFormat:@"-%@", [resource filenameOnDisk]];
-}
-
--(dispatch_queue_t)dispatchQueue {
-    static dispatch_queue_t _dispatch_queue = nil;
-    static dispatch_once_t _dispatch_once_t;
-    dispatch_once(&_dispatch_once_t, ^{
-        _dispatch_queue = dispatch_queue_create(kEkkoResourceManagerDispatchQueue , DISPATCH_QUEUE_CONCURRENT);
-    });
-    return _dispatch_queue;
 }
 
 @end
